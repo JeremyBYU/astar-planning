73ff4db95cc9c30e59e4163ebc35a618a0340ea5
Jeremy Castagno <Jeremy Castagno>
2016-11-05 23:31:49 -0400

    More data complete

--- src/astar.ts
+++ src/astar.ts
@@ -1,54 +1,97 @@
 import * as _ from 'lodash'
 import NodeT from './nodet'
-import { HeursticFunction } from './types'
+import { HeursticFunction, DistanceFunction, Cell, AStarResult } from './types'
+const PriorityQueue = require('js-priority-queue')
+
+
 
 
 /**
  * Astar
  */
 class AStar {
-    openList: Array<NodeT>
-    closedList: Array<NodeT>
-    heursticCostFunction: HeursticFunction
-    map: NodeT[][]
-    constructor(map: NodeT[][], heuristicCostFunction: HeursticFunction) {
+    openList: any                           // This will be a prioirty queue
+    closedList: NodeT[]                     // Simple array of a closed list
+    distanceCost: DistanceFunction          // A function that computes distance (user provided)
+    heursticCostEstimate: HeursticFunction  // A function that computes a heuristic estimate (user provided)
+    map: NodeT[][]                          // A map of nodes (user provided)
+    startNode: NodeT                        // The start node we are using
+    endGoalNode: NodeT                      // The end goal node we are trying to reach
+
+    constructor(map: NodeT[][], distanceCostFunction: DistanceFunction, heuristicFunction: HeursticFunction) {
         this.map = map
-        this.heursticCostFunction = heuristicCostFunction
+        this.heursticCostEstimate = heuristicFunction
+        this.distanceCost = distanceCostFunction
+        this.openList = new PriorityQueue( {comparator: this.compareNodes })
+    }
+
+
+    /**
+     * 
+     * 
+     * @param {Cell} start postion
+     * @param {Cell} goal position
+     * @returns {boolean} true if sucessful
+     * 
+     * @memberOf AStar
+     */
+    initializeSearch(start: Cell, goal: Cell): boolean {
+        this.startNode = this.findNode(start)
+        this.endGoalNode = this.findNode(goal)
+
+        const safe = (this.startNode !== null && this.endGoalNode !== null )
+        if (safe) {
+            // initialzation activities
+            this.openList.queue(this.startNode) // push on the start node on the queue
+        }
+        return safe;
+    }
+
+    stepSearchOnce() : AStarResult {
+        const bestOpenNode: NodeT = this.openList.peek() // get the best open node
+        // Check if bestOpenNode is the goal node
+        if (this.sameNode(bestOpenNode, this.endGoalNode)) {
+            this.endGoalNode.parent = bestOpenNode.parent
+            return AStarResult.FINISHED
+        }
+        this.openList.dequeue() // pop off this node, about to investigate all its neighbors
+        this.closedList.push(bestOpenNode) // ad this to the closed list
+        const neighbors = this.getNeighbors(bestOpenNode)
+        _.forEach(neighbors, (neighbor) => {
+            // Check if in closed list
+            // Check if in open list (crap how to do that with a priority queue) just use regular array with array.sort
+        })
+
         
     }
-    // /**
-    //  * 
-    //  * 
-    //  * @param {NodeT} Get all neighbor nodes around this node
-    //  * @returns {Array<NodeT>}
-    //  */
-    //  getNeighbors(node: NodeT): Array<NodeT> {
-    // }
+    sameNode(node1: NodeT, node2: NodeT): boolean {
+        return (node1.x === node2.x && node1.y === node2.y)
+    }
 
 
-    // /**
-    //  * 
-    //  * 
-    //  * @param {NodeT} node
-    //  * @param {NodeT} goal
-    //  * @returns {number}
-    //  */
-    //  heuristic_cost_estimate(node: NodeT, goal: NodeT): number {
-
-    // }
-
-
-    // /**
-    //  * 
-    //  * 
-    //  * @param {NodeT} prior
-    //  * @param {NodeT} next
-    //  * @returns {number}
-    //  */
-    //  cost_function(prior: NodeT, next: NodeT): number {
-
-    // }
-
+    /**
+     * 
+     * 
+     * @param {Cell} loc    - lcation to searh for 
+     * @returns {NodeT}     - Node in the mpa
+     * 
+     * @memberOf AStar
+     */
+    findNode(loc: Cell): NodeT {
+        if (loc.x > this.map.length || loc.y > this.map[0].length){
+            return null
+        }
+        return this.map[loc.x][loc.y]
+    }
+    /**
+     * 
+     * 
+     * @param {NodeT} Get all neighbor nodes around this node
+     * @returns {Array<NodeT>}
+     */
+     getNeighbors(node: NodeT): NodeT[] {
+         return []
+    }
 
     /**
      * 
@@ -56,8 +99,8 @@ class AStar {
      * @param {NodeT} node1
      * @param {NodeT} node2
      */
-     compareNodes(node1: NodeT, node2: NodeT) {
-
+     compareNodes(node1: NodeT, node2: NodeT): number {
+        return node1.f - node2.f
     }
 }
 
--- src/types.ts
+++ src/types.ts
@@ -10,4 +10,19 @@ export interface HeursticFunction {
     (node: NodeT): number
 }
 
-export type Map = { value: Array<Array<NodeT>> }
+export interface DistanceFunction {
+    (node1: NodeT, node2: NodeT): number
+}
+
+
+export interface Cell {
+    x: number
+    y: number
+}
+
+export enum AStarResult {
+    ERR = 1,
+    NO_PATH,
+    NORM,
+    FINISHED,
+}
